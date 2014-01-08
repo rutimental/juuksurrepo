ce4bf56340ec6215e0388e96599e50e9bf8d7aa6
Ruti Kerro <rutimental@gmail.com>
2014-01-08 21:25:01 +0200

    no message

--- src/salong/Salong.java
+++ src/salong/Salong.java
@@ -9,35 +9,39 @@ import salong.TextIO;
 public class Salong {
 
 	public static void main(String[] args) {
-		
+
 		// tühi string, mis täidetakse kasutaja sisestuga while tsüklis
-		String liige =  "";
-		
-		Juuksev2rv varv=new Juuksev2rv(); // varv on Juuksev2rv klassiobjekt
-		System.out.println("Pasol Nahhui :)?");
+		String liige = "";
+
+		Juuksev2rv varv = new Juuksev2rv(); // varv on Juuksev2rv klassiobjekt
+		System.out.println("ilusam :)?");
 		String sKasVarvime = TextIO.getlnString();
-		
-		if (sKasVarvime.equals("Jah")){
-			System.out.println("Davai värvime:"+varv.juuksev2rv()); // 
-		}else{System.out.println("Davai siis tõmba nahhui");}
-		
-		
+
+		if (sKasVarvime.equals("Jah")) {
+			System.out.println("Davai värvime:" + varv.juuksev2rv()); //
+		} else {
+			System.out.println("Davai siis tõmba nahhui");
+		}
+
 		while (true) {
-			
-			System.out.println("Sisesta kaardi nimi (Gold, Silver või puudub) : ");
-			
-			liige = Visiitkaart(); // liige saab uue väärtuse läbi Visiitkaart meetodi
+
+			System.out
+					.println("Sisesta kaardi nimi (Gold, Silver või puudub) : ");
+
+			liige = Visiitkaart(); // liige saab uue väärtuse läbi Visiitkaart
+									// meetodi
 
 			if ((liige.equals("Gold")) || ((liige.equals("Silver")))) {
-				
+
 				System.out.println("Sisestasid kliendikaardi.");
-				
+
 				break; // v2ljub while tsyklist kui sisestus on Silver v6i Gold
 			}
 			if (liige.equals("puudub")) {
-				
-				System.out.println("Kliendikaarti ei sisestatud, soodustused puuduvad.");
-				
+
+				System.out
+						.println("Kliendikaarti ei sisestatud, soodustused puuduvad.");
+
 				System.exit(1); // v2ljub programmist kui soodustused puuduvad
 			} else {
 
@@ -45,7 +49,7 @@ public class Salong {
 							// peale
 			}
 		}
-		
+
 		if ((liige.equals("Gold")) || ((liige.equals("Silver")))) {
 			System.out.println("Sisesta ostusumma : ");
 			int n = TextIO.getlnInt();
@@ -57,23 +61,23 @@ public class Salong {
 			}
 			JFrame frame1 = new JFrame(); // uus frame appletile
 			frame1.setSize(500, 500);
-			
-			//visiitkaart klassist tehakse objekt applet1
+
+			// visiitkaart klassist tehakse objekt applet1
 			final Applet applet1 = new Visiitkaart();
 			frame1.getContentPane().add(applet1);
 			frame1.setVisible(true);
-			
-			//applet1 ehk visiit kaardi applet init ja kävitamine
+
+			// applet1 ehk visiit kaardi applet init ja kävitamine
 			applet1.init();
 			applet1.start();
 		}
-		
-	}		
+
+	}
 
 	public static String Visiitkaart() {
-		
+
 		String liige = TextIO.getln();
-		
+
 		return liige;
 
 	}
